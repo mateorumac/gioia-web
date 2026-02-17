d201f5014ec9818430ddec358bf3e0387f949fe8
Mateo Rumac <mateo.rumac@gmail.com>
2026-02-17 10:03:48 +0100

    nearly finished gallery section

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,7 +1,23 @@
 import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/GallerySection.css";
-import { galleryImages } from "../utils/images";
+import patternBg from "../assets/green-pattern-bg.png";
+
+import img1 from "../assets/images/DSC_3662.webp";
+import img2 from "../assets/images/DSC_1517.webp";
+import img3 from "../assets/images/DSC_2478.webp";
+import img4 from "../assets/images/DSC_5385.webp";
+import img5 from "../assets/images/DSC_3690.webp";
+import img6 from "../assets/images/DSC_5624 2.webp";
+
+const galleryItems = [
+  { src: img1, alt: "Trening u studiju" },
+  { src: img2, alt: "Pilates trening" },
+  { src: img3, alt: "Prostor studija" },
+  { src: img4, alt: "Reformer trening" },
+  { src: img5, alt: "Studio detalj" },
+  { src: img6, alt: "Gioia studio ambijent" },
+];
 
 function GallerySection() {
   const { t } = useTranslation();
@@ -10,10 +26,13 @@ function GallerySection() {
   const currentLang = pathLang === "en" ? "en" : "hr";
   const galleryLink = currentLang === "en" ? "/en/gallery" : "/hr/galerija";
 
-  const images = galleryImages.slice(0, 7);
-
   return (
     <section className="gallerySection" id="gallery">
+      <div
+        className="gallerySection__patternLayer"
+        style={{ backgroundImage: `url(${patternBg})` }}
+      />
+
       <div className="gallerySection__container">
         <div className="gallerySection__header">
           <p className="gallerySection__eyebrow">Prostor studija</p>
@@ -23,16 +42,12 @@ function GallerySection() {
         </div>
 
         <div className="gallerySection__mosaic">
-          {images.map((src, index) => (
+          {galleryItems.map((item, index) => (
             <div
               key={index}
               className={`gallerySection__item gallerySection__item--${index + 1}`}
             >
-              <img
-                src={src}
-                alt={`Gioia studio ${index + 1}`}
-                loading="lazy"
-              />
+              <img src={item.src} alt={item.alt} loading="lazy" />
             </div>
           ))}
         </div>
